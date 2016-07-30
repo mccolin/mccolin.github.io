3a9925ed126e105a634021a3190446cedde794a1
Colin McCloskey <colin.mccloskey@gmail.com>
2016-07-30 01:09:41 -0400

    middleman-deploy configuration

--- config.rb
+++ config.rb
@@ -42,3 +42,14 @@ configure :build do
   # Minify Javascript on build
   activate :minify_javascript
 end
+
+# Deploy-specific configuration
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  deploy.commit_message = "Deployed version `version` by middleman-deploy at `timestamp`"
+end
