ebf835f44e226562a23492df366872f99f044a0f
Colin McCloskey <mccolin@users.noreply.github.com>
2023-08-12 17:27:10 -0400

    Update Middleman dependencies, tweak config, etc.

--- Gemfile
+++ Gemfile
@@ -3,15 +3,17 @@
 source 'https://rubygems.org'
 
 # For faster file watcher updates on Windows:
-gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
+#gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
+gem 'listen', '3.8.0'
+gem 'net-ftp', '0.2.0'
 
 # Windows does not come with time zone data
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 
 # Middleman Gems
-gem 'middleman', '>= 4.0.0'
-gem 'middleman-livereload'
-gem 'middleman-compass', '>= 4.0.0'
+gem 'middleman', '4.5.0'
+gem 'middleman-livereload', '3.4.7'
+gem 'middleman-compass','4.0.1'
 
 # Deploy to Github Pages:
-gem 'middleman-deploy', github: 'middleman-contrib/middleman-deploy', branch: 'master'
+#gem 'middleman-deploy' #, github: 'middleman-contrib/middleman-deploy', branch: 'master'
--- config.rb
+++ config.rb
@@ -44,12 +44,12 @@ configure :build do
 end
 
 # Deploy-specific configuration
-activate :deploy do |deploy|
-  deploy.deploy_method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  deploy.branch   = 'master' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-  deploy.commit_message = "Deploy changes by middleman-deploy"
-end
+# activate :deploy do |deploy|
+#   deploy.method = :git
+#   # Optional Settings
+#   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+#   deploy.branch   = 'master' # default: gh-pages
+#   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+#   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+#   deploy.commit_message = "Deploy changes by middleman-deploy"
+# end
